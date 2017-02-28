ca0ec05cfdfd6dc0204af23b9c78f0384ef9302b
Carlos Sánchez Cazorla <csanchez@bitnami.com>
2017-02-28 16:33:06 +0100

    @cscazorla joins the tournament :)

---
+++ bots/cscazorla/cscazorla.py
@@ -0,0 +1,22 @@
+import hlt
+from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
+import random
+
+myID, game_map = hlt.get_init()
+hlt.send_init("@csanchez")
+
+def assign_move(square):
+    for direction, neighbor in enumerate(game_map.neighbors(square)):
+        if neighbor.owner != myID and neighbor.strength < square.strength:
+            return Move(square, direction)
+
+    return Move(square, STILL)
+    # if square.strength < 5 * square.production:
+    #     return Move(square, STILL)
+    # else:
+    #     return Move(square, random.choice((NORTH, WEST)))
+
+while True:
+    game_map.get_frame()
+    moves = [assign_move(square) for square in game_map if square.owner == myID]
+    hlt.send_frame(moves)
